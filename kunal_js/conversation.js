223f7980635db69f9128c770445635dd693b8a72
kunaldambiwal <kunalkumhar207@gmail.com>
2025-02-17 18:54:13 +0000

    nvvv

---
+++ kunal_js/conversation.js
@@ -0,0 +1,6 @@
+let isloggedIn = "hskjh"
+let stringisloggedIn = String(isloggedIn)
+console.log(stringisloggedIn)
+console.log(typeof stringisloggedIn)
+let numberisloggedIn = Boolean(isloggedIn)
+console.log(numberisloggedIn)
